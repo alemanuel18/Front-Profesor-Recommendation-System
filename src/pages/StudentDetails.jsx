208d9642c1ff98a45866550424e3ce8b5231e478
alemanuel18 <jer24678@uvg.edu.gt>
2025-05-25 10:37:11 -0600

    Al momento de editar el perfil se autocompletan los datos ya registrados

--- src/pages/StudentDetails.jsx
+++ src/pages/StudentDetails.jsx
@@ -334,6 +334,7 @@ const StudentDetails = () => {
                                 isLoading={saving}
                                 error={error}
                                 initialData={student}
+                                isEditMode={true}
                             />
                         </div>
                     ) : (
